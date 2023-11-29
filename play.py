3992a6321db515c388739c0e45aec3b1f0e84879
tritan89 <61888066+tritan89@users.noreply.github.com>
2023-11-28 21:01:25 -0800

    good stats

--- play.py
+++ play.py
@@ -40,13 +40,13 @@ random_scores = np.load('random_scores.npy')
 ai_scores = np.load('ai_scores.npy')
 ai_mean = np.mean(ai_scores)
 ai_std = np.std(ai_scores)
-ai_mode = stats.mode(ai_scores)[0]
+ai_mode = stats.mode(ai_scores, keepdims=True)[0]
 ai_median = np.median(ai_scores)
 
 
 random_mean = np.mean(random_scores)
 random_std = np.std(random_scores)
-random_mode = stats.mode(random_scores)[0]
+random_mode = stats.mode(random_scores, keepdims=True)[0]
 random_median = np.median(random_scores)
 
 
@@ -55,23 +55,25 @@ types = ['mean', 'std', 'mode', 'median']
 ai_stats = [ai_mean, ai_std, ai_mode, ai_median]
 random_stats = [random_mean, random_std, random_mode, random_median]
 
-width = 0.25
+width1 = 0.25
 multiplier = 0
 x = np.arange(len(types))
 fig, axs = plt.subplots(2, 1, layout='constrained')
 
-for i in range(len(types)):
-    offset = width*multiplier
-    rects = axs[1].bar(types[i], ai_stats[i])
-    axs[1].bar_label(rects, padding=3)
-    width+=1 
+
+   
+axs[1].bar(x, ai_stats, width= width1, color = 'blue')
+axs[1].bar(x+0.25, random_stats,  width= width1, color = 'orange')
+axs[1].set_xticks(x + width1 )
+axs[1].set_xticklabels(types)
+axs[1].legend(['AI', 'Random'])
 
 
 axs[0].plot(ai_scores,  label = 'AI')
 axs[0].plot(random_scores, label = 'Random')
 axs[0].set_ylabel('score')
 axs[0].set_xlabel('hand number')
-
+axs[0].legend(['AI', 'Random'])
 
 
 plt.show()
